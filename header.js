341989d0b59c1fe95e57b3d35d0723be896ef7eb
proglearner <divaldo.sugianto@gmail.com>
2024-09-27 21:12:07 +0700

    v1.0

---
+++ header.js
@@ -0,0 +1,31 @@
+let nte = document.createElement ("div");
+document.body.appendChild (nte);
+nte.outerHTML = '<nav class="navbar navbar-expand-sm fixed-top" style="background-color:#8bc34a; padding:0;" id="navbar"><div class="logo"><img src="asset/logo.png" alt="logo" width="50" height="43"></div><div class="container-fluid justify-content-end"><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#header">≡</button><div class="collapse navbar-collapse justify-content-end" id="header"><ul class="navbar-nav"></ul></div></div></nav><div id="header_trick"></div>';
+document.querySelector ("#header_trick").style.height = String (document.querySelector ("#navbar").clientHeight) + "px";
+const navopt = document.querySelector (".navbar-nav");
+const path = String(document.location).split ("/");
+let docname = path[path.length-1];
+docname = docname.split ("#");
+docname = docname[0];
+if (docname == "index.html") {
+ navopt.innerHTML = '<li class="nav-item"><a class="nav-link" href="#">Home</a></li><li class="nav-item"><a class="nav-link" href="#about">About Us</a></li><li class="nav-item"><a class="nav-link" href="#testimoni">Testimoni</a></li><li class="nav-item"><a class="nav-link" href="#artikel">Artikel</a></li><li class="nav-item"><a class="nav-link" id="acc_home"></a></li>';
+ const acc_home = document.querySelector ("#acc_home");
+ if (sessionStorage.getItem("account") != null) {
+  acc_home.innerHTML = "Dashboard";
+  acc_home.href = "dashboard.html";
+ } else {
+  acc_home.innerHTML = "Login";
+  acc_home.href = "#login";
+ }
+} else {
+ navopt.innerHTML = '<li class="nav-item"><a class="nav-link" href="index.html">Home</a></li><li class="nav-item"><span id="logout" class="nav-link">Logout</span></li>'
+ const logout = document.querySelector ("#logout");
+ logout.addEventListener ("click", function logout () {
+  sessionStorage.clear();
+  document.location = "index.html";
+ });
+}
+const favicon = document.createElement ("link");
+favicon.rel = "icon";
+favicon.href = "asset/logo.png";
+document.head.appendChild (favicon);
---
+++ js/pointjs.js
@@ -0,0 +1,33 @@
+const banana = document.querySelector ("#a500");
+const milk = document.querySelector ("#a1000");
+const ticket = document.querySelector ("#a5000");
+const rewards = [banana, milk, ticket];
+rewards.forEach (function (item) {
+ item.addEventListener ("click", function () {
+  let pts = item.id;
+  pts = pts.replace ("a", "");
+  pts = Number(pts);
+  if (sessionStorage.getItem('point') >= pts) {
+   sessionStorage.setItem('point', sessionStorage.getItem('point')-pts);
+  }
+ });
+});
+pbcs = [];
+rewards.forEach (function (item) {
+ let pbc = document.createElement ("span");
+ pbc.className = "pbc";
+ pbcs.push (pbc);
+ item.appendChild (pbc);
+});
+function updatePoint () {
+ document.querySelector ("#total_point").innerHTML = sessionStorage.getItem ("point");
+ pbcs.forEach (function (item) {
+  if (Number(String (item.parentElement.id).replace("a", "")) > sessionStorage.getItem("point")) {
+   item.innerHTML = "<br>Yah... Pointmu belum cukup untuk mendapatkan aku";
+  } else {
+   item.innerHTML = "<br>Klik gambar untuk mendapatkan aku!"
+  }
+ });
+ requestAnimationFrame (updatePoint);
+}
+requestAnimationFrame (updatePoint);
