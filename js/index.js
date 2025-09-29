880a1889eb2e4107b0798b554b8d9669526ed25b
proglearner <divaldo.sugianto@gmail.com>
2025-09-29 17:42:34 +0700

    Face Recognition & Design Refines

--- js/index.js
+++ js/index.js
@@ -29,3 +29,92 @@ artikel_btn.addEventListener ("click", function () {
     artikel_btn.innerHTML = "Selengkapnya";
   }
 });
+
+Promise.all([
+  faceapi.nets.ssdMobilenetv1.loadFromUri("face-api.js-master/weights"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("face-api.js-master/weights"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("face-api.js-master/weights"),
+  faceapi.nets.ageGenderNet.loadFromUri("face-api.js-master/weights")
+])//.then(startVideo)
+
+/*
+  [min, max, menu]
+  min <= u < max
+*/
+const recommendation = [
+  [0, 10, "asset/STEAK_DETAIL.png"],
+  [10, 20, "asset/RENDANG_DETAIL.png"],
+  [20, 30, "asset/BROKOLI_DETAIL.png"],
+  [25, 30, "asset/APEL_DETAIL.png"],
+  [30, 40, "asset/KANGKUNG_DETAIL.png"],
+  [40, Number.POSITIVE_INFINITY, "asset/PISANG_DETAIL.png"]
+]
+
+const video = document.querySelector("#video");
+const recomm = document.querySelector("#recommendation");
+video.addEventListener("play", function(){
+    interval_ids.push(setInterval(async function(){
+      const detections = await faceapi.detectAllFaces(video, new faceapi.SsdMobilenetv1Options()).withAgeAndGender();
+      if(detections.length > 0) {
+        console.log(detections);
+        stopVideo();
+        video.style.display = "none";
+        recomm.style.display = "block";
+        for (let r = 0; r < recommendation.length; r++){
+          rc = recommendation[r];
+          if(rc[0] <= detections[0].age && detections[0].age < rc[1]){
+            recomm.src = rc[2];
+            break;
+          }
+          console.log(rc)
+          document.querySelector("#scan_description").innerText = 
+            "Rekomendasi Menu untuk Anda";
+        }
+      }
+    }, 100))
+  })
+let video_streams = [] // one time to prevent unaccessible streams
+let interval_ids = [] // to handle multiple play (unexpected)
+let timeouts = []
+function startVideo(){
+  timeouts.forEach(function(item){clearTimeout(item);});
+  navigator.mediaDevices.getUserMedia({video: {}}).then(
+    function(stream){
+      video.srcObject = stream;
+      video_streams.push(stream);
+      console.log(stream);
+    }, 
+    function(err){console.error(err)});
+  video_streams.push(false);
+  video.style.display = "block";
+  recomm.style.display = "none";
+  video.play()
+}
+const modal = document.querySelector("#MenuAI_box");
+modal.addEventListener("hidden.bs.modal", stopVideo);
+function stopVideo(){
+  interval_ids.forEach(function(id){
+    clearInterval(id)
+  });
+  interval_ids = [];
+  video_streams.forEach(function(stream){
+    if(stream){
+      stream.getTracks().forEach(function(track){
+        if(track.readyState == "live") {track.stop();}
+      });
+    }
+  });
+  video.pause();
+  video.srcObject = null;
+}
+document.querySelector("#MenuAI_btn").addEventListener("click", startVideo)
+timeouts.push(setTimeout(function(){
+  m = new bootstrap.Modal(modal)
+  m.show()
+  startVideo();
+}, 5000));
+recomm.addEventListener("click", function(){
+  if(sessionStorage.getItem("account") != null){
+    document.location = "dashboard.html"
+  }
+})
